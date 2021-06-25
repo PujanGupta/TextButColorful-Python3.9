e2a1ee679cd4ed0e9a76d2789490b122c5238f32
PujanGupta <86420213+PujanGupta@users.noreply.github.com>
2021-06-25 09:38:18 +0800

    Add files via upload

---
+++ ColoredText.py
@@ -0,0 +1,37 @@
+from colorama import Fore
+
+
+def setFontColorRed(text):
+    return Fore.RED + text
+
+
+def setFontColorGreen(text):
+    return Fore.GREEN + text
+
+
+def setFontColorBlue(text):
+    return Fore.BLUE + text
+
+
+def setFontColorCyan(text):
+    return Fore.CYAN + text
+
+
+def setFontColorMagenta(text):
+    return Fore.MAGENTA + text
+
+
+def setFontColorGold(text):
+    return Fore.YELLOW + text
+
+
+def setFontColorWhite(text):
+    return Fore.WHITE + text
+
+
+def setFontColorBlack(text):
+    return Fore.BLACK + text
+
+
+def setFontColorLightRed(text):
+    return Fore.LIGHTRED_EX + text
